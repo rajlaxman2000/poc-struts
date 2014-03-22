627bfa04af43e19648a77af1223c61675b600ded
kanasco.erp@gmail.com <kanasco.erp@gmail.com@e55238d2-90cd-4940-9952-98d26e020b56>
2014-03-22 10:20:17 +0000

    log4j

---
+++  poc-struts/Log4jExample1/src/com/vaannila/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.vaannila.helloworld;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class HelloWorld {
+
+	static final Logger logger = Logger.getLogger(HelloWorld.class);
+	
+	public static void main(String[] args) {
+		BasicConfigurator.configure();
+		/*PropertyConfigurator.configure("log4j.properties");*/
+		logger.debug("Sample debug message");
+		logger.info("Sample info message");
+		logger.warn("Sample warn message");
+		logger.error("Sample error message");
+		logger.fatal("Sample fatal message");
+	}
+}
